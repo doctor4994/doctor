65e7a2a2c673504ecd9bee7bf02f817aca338db1
doctor4994 <doctor4994@outlook.com>
2017-05-22 23:47:16 +0500

    source file

---
+++ src/main/java/com/example/doctor/loginprofile/EditUserProfile.java
@@ -0,0 +1,13 @@
+package com.example.doctor.loginprofile;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class EditUserProfile extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_edit_user_profile);
+    }
+}
